86fb4b295b0af7d3ca88fd856e170642c9e6e98f
malek <malekgoku@gmail.com>
2024-11-04 22:45:16 -0500

    add the coding challenge

---
+++ cyber.js
@@ -0,0 +1,17 @@
+const talentData = [
+    {
+      expLevel: 'entry-level',
+      openJobs: Math.floor(Math.random() *   21),
+      avTalent: 25
+    },
+    {
+      expLevel: 'mid-level',
+      openJobs: Math.floor(Math.random() * 21),
+      avTalent: 20
+    },
+    {
+      expLevel: 'senior-level',
+      openJops: Math.floor(Math.random() * 21),
+      avTalent: 10
+    }
+  ];
